7418a8e1d9d5b892ee41cb081c67f479e2095ef4
rolexye <rolexye@rolexye.(none)>
2013-09-12 00:19:59 +0800

    nn

--- neural_network_test.c
+++ neural_network_test.c
@@ -4,7 +4,7 @@
 #include<time.h>
 #include<string.h>
 
-#define D 8
+#define D 7
 #define N 200
 #define M 10
 #define K 2
@@ -14,13 +14,13 @@
 
 #define rando() (rand() * 1.0 / RAND_MAX)
 
-double w[layer][M][M];
-double x[N][D];
-double a[M], z[M];
-double y[N][K];
-double t[N][K];
-double err[layer][M];
-double d[layer][M][M];
+double w[layer][M+1][M+1];
+double x[N][D+1];
+double a[M+1], z[M+1];
+double y[N][K+1];
+double t[N][K+1];
+double err[layer][M+1];
+double d[layer][M+1][M+1];
 
 double tanh(double a){
     return (exp(a) - exp(-a)) / (exp(a) + exp(-a));
@@ -34,24 +34,22 @@ double sigmoid(double a){
     return 1.0 / (1 + exp(-a)); 
 }
 
-double feed_forward(double *in, int in_cnt, double *out, int out_cnt, double param[M][M], double (*trans_method)(double)){
+double feed_forward(double *in, int in_cnt, double *out, int out_cnt, double param[M+1][M+1], double (*trans_method)(double)){
     int i, j;
-    double a[M];
-    for(i = 0; i < out_cnt; i++){
-        a[i] = 0;
-        for(j = 0; j < in_cnt; j++){
+    double a[M+1];
+    for(i = 1; i <= out_cnt; i++){
+        a[i] = param[i][0];
+        for(j = 1; j <= in_cnt; j++){
             a[i] += param[i][j] * in[j];
         }
         out[i] = trans_method(a[i]);
     }
 }
 
-double* predict(double in[D]){
+void predict(double in[D+1], double out[K+1]){
     double hidden_unit[M];
-    double *ret = malloc(K * sizeof(double)); 
     feed_forward(in, D, hidden_unit, M, w[0], sigmoid);
-    feed_forward(hidden_unit, M, ret, K, w[1], sigmoid);
-    return ret;
+    feed_forward(hidden_unit, M, out, K, w[1], sigmoid);
 }
 
 double get_total_error(int n){
@@ -70,7 +68,7 @@ int main(){
     double d5, d6;
     int loop;
     double total_error;
-    double *test;
+    double test[K+1];
     double in1[8] = {1, 6, 148, 72, 35, 33.6, 0.627, 50}; //Yes
     double in2[8] = {1, 1, 85, 66, 29, 26.6, 0.351, 31}; //No
 
@@ -98,8 +96,8 @@ int main(){
     }
 
     for(k = 0; k < layer; k++)
-        for(i = 0; i < M; i++)
-            for(j = 0; j < M; j++){
+        for(i = 0; i <= M; i++)
+            for(j = 0; j <= M; j++){
                 d[k][i][j] = 0;
                 w[k][i][j] = 2.0 * (rando() - 0.5) * 0.1;
             }
@@ -108,34 +106,37 @@ int main(){
         total_error = 0;
         for(n = 0; n < N; n++){
             feed_forward(x[n], D, z, M, w[0], sigmoid);
-            feed_forward(z, M, y[n], 2, w[1], sigmoid);
+            feed_forward(z, M, y[n], K, w[1], sigmoid);
             total_error += get_total_error(n); 
-            for(i = 0; i < K; i++){
+            for(i = 1; i <= K; i++){
                 err[1][i] = (t[n][i] - y[n][i]) * y[n][i] * (1.0 -y[n][i]);
             }
-            for(i = 0; i < M; i++){
+            for(i = 1; i <= M; i++){
                 err[0][i] = 0;
-                for(j = 0; j < K; j++){
+                for(j = 0; j <= K; j++){
                     err[0][i] += w[1][j][i] * err[1][j]; 
                 }
                 err[0][i] *= z[i] * (1.0 - z[i]);
             }
-            for(j = 0; j < M; j++)
-                for(i = 0; i < D; i++){
+            for(j = 1; j <= M; j++)
+                d[0][j][0] = alpha * d[0][j][0] + eta * err[0][j];
+                w[0][j][0] += d[0][j][0];
+                for(i = 0; i <= D; i++){
                     d[0][j][i] = alpha * d[0][j][i] + eta * err[0][j] * x[n][i];
                     w[0][j][i] += d[0][j][i]; 
                 }
-            for(k = 0; k < K; k++)
-                for(j = 0; j < M; j++){
-                    d[1][k][j] = alpha * d[1][k][j] + eta * err[1][j] * z[j];
+            for(k = 1; k <= K; k++)
+                d[1][k][0] = alpha * d[1][k][0] + eta * err[1][k];
+                w[1][k][0] += d[1][k][0];
+                for(j = 1; j <= M; j++){
+                    d[1][k][j] = alpha * d[1][k][j] + eta * err[1][k] * z[j];
                     w[1][k][j] += d[1][k][j];
                 }
         }
     }
-    test = predict(in1);
-    printf("%lf\t%lf\n", test[0], test[1]);
-    test = predict(in2);
-    printf("%lf\t%lf\n", test[0], test[1]);
-    free(test);
+    predict(in1, test);
+    printf("%lf\t%lf\n", test[1], test[2]);
+    predict(in2, test);
+    printf("%lf\t%lf\n", test[1], test[2]);
     return 0;
 }
